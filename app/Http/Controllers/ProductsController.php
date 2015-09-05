12e376c04e17aa83b775d02607c7d3b77455a24e
Maico Machado <maicoamachado@gmail.com>
2015-09-04 22:22:41 -0300

    Upload de Imagens

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,10 +4,12 @@ namespace CodeCommerce\Http\Controllers;
 
 use CodeCommerce\Category;
 use CodeCommerce\Product;
-use Illuminate\Http\Request;
+use CodeCommerce\ProductImage;
+use Illuminate\Http\RequProductImageRequestest;
 use CodeCommerce\Http\Requests\ProductRequest;
 use CodeCommerce\Http\Requests;
-use CodeCommerce\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class ProductsController extends Controller
 {
@@ -60,5 +62,37 @@ class ProductsController extends Controller
 
         return redirect()->route('products');
 
+    }
+
+    public function images($id){
+        $product = $this->productModel->find($id);
+        return view('products.images', compact('product'));
+    }
+
+    public function createImage($id){
+        $product = $this->productModel->find($id);
+        return view('products.create_image', compact('product'));
+    }
+
+    public function storeImage(ProductImageRequest $request, $id, ProductImage $productImage){
+
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+        $image = $productImage::create(['product_id' => $id, 'extension' => $extension]);
+
+        Storage::disk('public_local')->put($image->id.'.'.$extension, File::get($file));
+
+        return redirect()->route('products.images', ['id' => $id]);
+    }
+
+    public function destroyImage(ProductImage $productImage, $id){
+        $image = $productImage->find($id);
+        if(file_exists(public_path().'/uploads/'.$image->id.'.'.$image->extension))
+            Storage::disk('public_local')->delete($image->id.'.'.$image->extension);
+
+        $product = $image->product;
+        $image->delete();
+
+        return redirect()->route('products.images', ['id' => $product->id]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -131,13 +131,22 @@ Route::group(['prefix' => 'admin', 'where' => ['id' => '[0-9]+']], function(){
     });
 
     Route::group(['prefix' => 'products'], function(){
-    Route::get('/',['as' => 'products', 'uses' => 'ProductsController@index']);
-    Route::post('/',['as' => 'products.store', 'uses' =>'ProductsController@store']);
-    Route::get('create',['as' => 'products.create', 'uses' => 'ProductsController@create']);
-    Route::get('{id}/destroy',['as' => 'products.destroy', 'uses' => 'ProductsController@destroy']);
-    Route::get('{id}/edit',['as' => 'products.edit', 'uses' => 'ProductsController@edit']);
-    Route::put('{id}/update',['as' => 'products.update', 'uses' => 'ProductsController@update']);
+        Route::get('/',['as' => 'products', 'uses' => 'ProductsController@index']);
+        Route::post('/',['as' => 'products.store', 'uses' =>'ProductsController@store']);
+        Route::get('create',['as' => 'products.create', 'uses' => 'ProductsController@create']);
+        Route::get('{id}/destroy',['as' => 'products.destroy', 'uses' => 'ProductsController@destroy']);
+        Route::get('{id}/edit',['as' => 'products.edit', 'uses' => 'ProductsController@edit']);
+        Route::put('{id}/update',['as' => 'products.update', 'uses' => 'ProductsController@update']);
+
+        Route::group(['prefix' => 'images'], function(){
+            Route::get('{id}/product',['as' => 'products.images', 'uses' => 'ProductsController@images']);
+            Route::get('create/{id}/product',['as' => 'products.images.create', 'uses' => 'ProductsController@createImage']);
+            Route::post('store/{id}/product',['as' => 'products.images.store', 'uses' =>'ProductsController@storeImage']);
+            Route::get('destroy/{id}/product',['as' => 'products.images.destroy', 'uses' =>'ProductsController@destroyImage']);
+        });
 
     });
+
+
 });
 
